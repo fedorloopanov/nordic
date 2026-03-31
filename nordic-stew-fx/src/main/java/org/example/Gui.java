7deae7c726e812097b85568974238b1e94919e1d
fedorloopanov <zoranyovovich@gmail.com>
2026-03-31 11:36:47 +0300

    Update and rename App.java to Gui.java

--- nordic-stew-fx/src/main/java/org/example/Gui.java
+++ nordic-stew-fx/src/main/java/org/example/Gui.java
@@ -11,7 +11,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class Gui extends Application {
 
     private final ObservableList<String> history = FXCollections.observableArrayList();
 
