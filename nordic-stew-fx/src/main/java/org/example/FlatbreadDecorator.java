df1ee1c7415b2946848dd4bfe2f3487ac21fd88a
fedorloopanov <zoranyovovich@gmail.com>
2026-03-31 18:44:08 +0300

    Update FlatbreadDecorator.java

--- nordic-stew-fx/src/main/java/org/example/FlatbreadDecorator.java
+++ nordic-stew-fx/src/main/java/org/example/FlatbreadDecorator.java
@@ -1,17 +1,18 @@
 package org.example;
 
 public class FlatbreadDecorator extends DishDecorator {
+
     public FlatbreadDecorator(Dish dish) {
         super(dish);
     }
 
     @Override
     public String getName() {
-        return dish.getName() + " + Нордская лепешка";
+        return getDish().getName() + " + Нордская лепешка";
     }
 
     @Override
     public int getPrice() {
-        return dish.getPrice() + 7;
+        return getDish().getPrice() + 7;
     }
 }
