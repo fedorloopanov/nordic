35454aab43d135bf575290af42102dd073662271
fedorloopanov <zoranyovovich@gmail.com>
2026-03-31 18:41:17 +0300

    Update DishDecorator.java

--- nordic-stew-fx/src/main/java/org/example/DishDecorator.java
+++ nordic-stew-fx/src/main/java/org/example/DishDecorator.java
@@ -1,9 +1,28 @@
 package org.example;
 
 public abstract class DishDecorator implements Dish {
-    protected Dish dish;
+
+    private Dish dish;
 
     public DishDecorator(Dish dish) {
         this.dish = dish;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    @Override
+    public String getName() {
+        return dish.getName();
+    }
+
+    @Override
+    public int getPrice() {
+        return dish.getPrice();
     }
 }
