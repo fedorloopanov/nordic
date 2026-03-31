a01bb1bca26d1ab6177b99268a90e25a1da0e45f
fedorloopanov <zoranyovovich@gmail.com>
2026-03-31 18:42:06 +0300

    Update FireSauceDecorator.java

--- nordic-stew-fx/src/main/java/org/example/FireSauceDecorator.java
+++ nordic-stew-fx/src/main/java/org/example/FireSauceDecorator.java
@@ -1,17 +1,18 @@
 package org.example;
 
 public class FireSauceDecorator extends DishDecorator {
+
     public FireSauceDecorator(Dish dish) {
         super(dish);
     }
 
     @Override
     public String getName() {
-        return dish.getName() + " + Огненный соус";
+        return getDish().getName() + " + Огненный соус";
     }
 
     @Override
     public int getPrice() {
-        return dish.getPrice() + 10;
+        return getDish().getPrice() + 10;
     }
 }
